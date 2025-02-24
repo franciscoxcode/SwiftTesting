2964efda27dee1d647fc32bb6335e70cbd187697
Francisco Casillas <chiccasillas@gmail.com>
2025-02-24 11:02:08 -0600

    Testing with personalized views

--- SwiftTesting/ContentView.swift
+++ SwiftTesting/ContentView.swift
@@ -9,15 +9,40 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        HStack{
+            ProfileCardView(name: "Max", role: "Engineer", imageName: "him")
+            ProfileCardView(name: "Benja", role: "Engineer",imageName: "him")
         }
-        .padding()
     }
 }
+
+struct ProfileCardView: View {
+    var name: String
+    var role: String
+    var imageName: String
+
+    var body: some View {
+        VStack {
+            Image(imageName)
+                .resizable()
+                .scaledToFit()
+                .frame(width: 100, height: 100)
+                .clipShape(Circle())
+
+            Text(name)
+                .font(.headline)
+
+            Text(role)
+                .font(.subheadline)
+                .foregroundColor(.gray)
+        }
+        .padding()
+        .background(Color.white)
+        .cornerRadius(10)
+        .shadow(radius: 5)
+    }
+}
+
 
 #Preview {
     ContentView()
