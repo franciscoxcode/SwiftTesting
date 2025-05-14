cd05cbaf1e049f8222ba4bff9ae937ae99124c06
Francisco Casillas <chiccasillas@gmail.com>
2025-05-14 08:56:08 -0600

    Display all font sizes for book

--- SwiftTesting/ContentView.swift
+++ SwiftTesting/ContentView.swift
@@ -2,21 +2,36 @@ import SwiftUI
 
 struct KeyboardToolbarView: View {
     var body: some View {
-        NavigationView {
-            Text("Hola, Max")
-                .navigationTitle("Inicio")
-                .toolbar {
-                    ToolbarItem(placement: .navigationBarTrailing) {
-                        Button(action: {
-                            print("Botón presionado")
-                        }) {
-                            Image(systemName: "gear")
-                        }
-                    }
-                }
+        VStack {
+            Text(".largetTitle")
+                .font(.largeTitle)
+            Text(".title")
+                .font(.title)
+            Text(".title2")
+                .font(.title2)
+            Text(".title3")
+                .font(.title3)
+            Text(".headline")
+                .font(.headline)
+            Text(".subheadline")
+                .font(.subheadline)
+            Text(".body")
+                .font(.body)
+            Text(".footnote")
+                .font(.footnote)
+            Text(".caption")
+                .font(.caption)
         }
+//        - `.largeTitle`: Tamaño muy grande (como un encabezado).
+//        - `.title`: Tamaño de título estándar.
+//        - `.headline`: Tamaño usado para encabezados pequeños.
+//        - `.subheadline`: Tamaño para subtítulos.
+//        - `.body`: Tamaño estándar para texto en párrafos.
+//        - `.footnote`: Tamaño pequeño.
+//        - `.caption`: Tamaño muy pequeño, ideal para descripciones.
     }
 }
+
 
 #Preview {
     KeyboardToolbarView()
