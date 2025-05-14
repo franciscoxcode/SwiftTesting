cce98903854c74b917fd9685815df6644f6a02fe
Francisco Casillas <chiccasillas@gmail.com>
2025-05-14 12:16:30 -0600

    Modify image with all different kind of modifiers for book

--- SwiftTesting/ContentView.swift
+++ SwiftTesting/ContentView.swift
@@ -3,32 +3,17 @@ import SwiftUI
 struct KeyboardToolbarView: View {
     var body: some View {
         VStack {
-            Text(".largetTitle")
-                .font(.largeTitle)
-            Text(".title")
-                .font(.title)
-            Text(".title2")
-                .font(.title2)
-            Text(".title3")
-                .font(.title3)
-            Text(".headline")
-                .font(.headline)
-            Text(".subheadline")
-                .font(.subheadline)
-            Text(".body")
-                .font(.body)
-            Text(".footnote")
-                .font(.footnote)
-            Text(".caption")
-                .font(.caption)
-        }
-//        - `.largeTitle`: Tamaño muy grande (como un encabezado).
-//        - `.title`: Tamaño de título estándar.
-//        - `.headline`: Tamaño usado para encabezados pequeños.
-//        - `.subheadline`: Tamaño para subtítulos.
-//        - `.body`: Tamaño estándar para texto en párrafos.
-//        - `.footnote`: Tamaño pequeño.
-//        - `.caption`: Tamaño muy pequeño, ideal para descripciones.
+                    Image("robot")
+                        .resizable()
+                        .scaleEffect(x: 0.5, y: 0.3)
+                        .frame(width: 300, height: 500)
+                        .border(Color.green, width: 3)
+
+                    Text("")
+                        .padding()
+                        .font(.headline)
+                }
+                .padding()
     }
 }
 
