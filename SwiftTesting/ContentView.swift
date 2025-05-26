11d6d5ce11379afcdd7a79778d0afced6a85280a
Francisco Casillas <chiccasillas@gmail.com>
2025-05-26 12:41:12 -0600

    Testing different types of buttons

--- SwiftTesting/ContentView.swift
+++ SwiftTesting/ContentView.swift
@@ -2,32 +2,15 @@ import SwiftUI
 
 struct KeyboardToolbarView: View {
     var body: some View {
-        VStack {
-                    Image("robot")
-                        .resizable()
-                        .frame(width: 300, height: 300)
-                        .border(Color.green, width: 3)
-                        .overlay(
-                            Text("I love you!")
-                                .font(.title)
-                                .foregroundColor(.red)
-                                .padding()
-                            , alignment: .topLeading
-                        )
-                        .overlay(
-                            Image(systemName: "star.fill")
-                                .foregroundColor(.yellow)
-                                .padding()
-                            , alignment: .bottomTrailing
-                        )
-
-                    Text("")
-                        .padding()
-                        .font(.headline)
-                }
-                .padding()
+        Button {
+            print("Settings tapped")
+        } label: {
+            Label("Settings", systemImage: "gearshape.fill")
+        }
+        .buttonStyle(.automatic)
     }
 }
+
 
 
 #Preview {
