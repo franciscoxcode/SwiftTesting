a44ae5491cf14746ea39bce325fbdeacccfccdfc
Francisco Casillas <chiccasillas@gmail.com>
2025-06-17 12:10:01 -0600

    Create a simple exercise using @EnvironmentObject

--- SwiftTesting/ContentView.swift
+++ SwiftTesting/ContentView.swift
@@ -1,32 +1,5 @@
 import SwiftUI
 
-// ContentView displays a simple counter with plus and minus buttons.
-// The counter's value is stored in a StateObject of CounterViewModel.
-
-struct ContentView: View {
-    @StateObject var counterview = CounterViewModel()
-
-    var body: some View {
-        HStack {
-            // Tapping this button decreases the counter.
-            Button(action: {
-                counterview.subtractOne()
-            }){
-                Image(systemName: "minus")
-            }
-            Text("\(counterview.counter)")
-                .padding()
-            // Tapping this button increases the counter.
-            Button(action: {
-                counterview.addOne()
-            }){
-                Image(systemName: "plus")
-            }
-        }
-        .font(.system(size: 25))
-    }
-}
-
 class CounterViewModel: ObservableObject {
     @Published var counter = 0
     
@@ -35,30 +8,45 @@ class CounterViewModel: ObservableObject {
     }
 }
 
-struct ContentView: View {     
+struct ContentView: View {
     @StateObject var viewModel = CounterViewModel()
+
+    var body: some View {
+        HStack{
+            CounterIncrementView()
+            CounterDisplayView()
+            CounterResetView()
+            
+        }
+            .environmentObject(viewModel)
+    }
+}
+
+struct CounterDisplayView: View {
+    @EnvironmentObject var viewModel: CounterViewModel
+    var body: some View {
+        Text("\(viewModel.counter)")
+    }
+}
+
+struct CounterIncrementView: View {
+    @EnvironmentObject var viewModel: CounterViewModel
     
     var body: some View {
-        VStack {
-            SecondView(viewModel: viewModel)
+        Button("+"){
+            viewModel.counter += 1
         }
     }
 }
 
-struct SecondView: View {
-    @ObservedObject var viewModel: CounterViewModel
+struct CounterResetView: View {
+    @EnvironmentObject var viewModel: CounterViewModel
     var body: some View {
-        HStack {
-            Text("\(viewModel.counter)")
-            Button("+"){
-                viewModel.increment()
-            }
+        Button("reset"){
+            viewModel.counter = 0
         }
     }
 }
-
-
-
 
 #Preview {
     ContentView()
