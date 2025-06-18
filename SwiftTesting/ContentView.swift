f893b70db46c4fc478e6a682481a2d23ec390c25
Francisco Casillas <chiccasillas@gmail.com>
2025-06-18 17:09:24 -0600

    test: testing different types of forms

--- SwiftTesting/ContentView.swift
+++ SwiftTesting/ContentView.swift
@@ -1,56 +1,22 @@
 import SwiftUI
 
-struct Ingredient: Identifiable {
-    let id = UUID()
-    let name: String
-    let category: String
-    let image: String
-}
-
 struct ContentView: View {
-    
-    let ingredients: [Ingredient] = [
-        Ingredient(name: "carrot", category: "vegetable", image: "🥕"),
-        Ingredient(name: "broccoli", category: "vegetable", image: "🥦"),
-        Ingredient(name: "lettuce", category: "vegetable", image : "🥬"),
-        Ingredient(name: "apple", category: "fruit", image: "🍎"),
-        Ingredient(name: "banana", category: "fruit", image: "🍌"),
-        Ingredient(name: "strawberry", category: "fruit", image: "🍓"),
-        Ingredient(name: "cake", category: "dessert", image: "🍰"),
-        Ingredient(name: "cupcake", category: "dessert", image: "🧁"),
-        Ingredient(name: "flan", category: "dessert", image: "🍮")
-    ]
-    
-var body: some View {
-    List{
-        Section(header: Text("Vegetables")) {
-            ForEach(ingredients.filter {$0.category == "vegetable"}){ ingredient in
-                HStack {
-                    Text(ingredient.image) 
-                    Text("\(ingredient.name)")
-                }
+    @State private var text1: String = ""
+    @State private var text2: String = ""
+
+    var body: some View {
+        Form {
+            VStack {
+                TextField("Default", text: $text1)
+                    .textFieldStyle(DefaultTextFieldStyle())
+
+                TextField("Plain", text: $text2)
+                    .textFieldStyle(PlainTextFieldStyle())
             }
+            .padding()
         }
-        Section(header: Text("Fruits")){
-            ForEach(ingredients.filter {$0.category == "fruit" }){ ingredient in
-                HStack {
-                    Text(ingredient.image) 
-                    Text("\(ingredient.name)")
-                }
-            }
-        }
-        Section(header: Text("Desserts")){
-            ForEach(ingredients.filter {$0.category == "dessert"}){ ingredient in 
-                HStack {
-                    Text(ingredient.image) 
-                    Text("\(ingredient.name)")
-                }
-            }
-        }
-    }
     }
 }
-
 #Preview {
     ContentView()
 }
