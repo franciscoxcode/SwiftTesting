15f7c25a2247f8596fd813438a157720bd4dba8c
Francisco Casillas <chiccasillas@gmail.com>
2025-08-20 11:16:42 -0600

    practice: NavigationStack and NavigationLink in multiple levels

--- SwiftTesting/ContentView.swift
+++ SwiftTesting/ContentView.swift
@@ -3,18 +3,27 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         NavigationStack {
-            NavigationLink("Go to profile"){
-                ProfileView()
+            NavigationLink("Level 2"){
+                Level2View()
             }
-            .navigationTitle("Home")
+            .navigationTitle("Level 1")
         }
     }
 }
 
-struct ProfileView: View {
+struct Level2View: View {
     var body: some View {
-        Text("This is the profile screen")
-            .navigationTitle("Profile")
+            NavigationLink("Level 3"){
+                Level3View()
+            }
+            .navigationTitle("Level 2")
+    }
+}
+
+struct Level3View: View {
+    var body: some View {
+        Text("This is Level 3")
+            .navigationTitle("Level 3")
     }
 }
 
