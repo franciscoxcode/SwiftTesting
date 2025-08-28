a840f2f3b4b8b750deebe62654af1ebc368bf121
Francisco Casillas <chiccasillas@gmail.com>
2025-08-28 17:33:28 -0600

    practice: add counter with shared state across two screens

--- SwiftTesting/ContentView.swift
+++ SwiftTesting/ContentView.swift
@@ -1,32 +1,43 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State var name: String = ""
-    @State var isPublic: Bool = false
+    
+    @State var count = 0
     
     var body: some View {
         NavigationStack {
             Form {
-                TextField("Name", text: $name)
-                Toggle("Public Profile", isOn: $isPublic)
+                Section(header: Text("Main")){
+                    HStack {
+                        Text("\(count)")
+                        Button("+"){
+                            count += 1
+                        }
+                    }
+                }
+                Section(header: Text("Navigation")) {
+                    NavigationLink("B Screen", destination: BScreen(count: $count))
+                }
             }
-            NavigationLink("Submit", destination: ProfileDetailView(name: $name, isPublic: $isPublic))
         }
     }
 }
 
-struct ProfileDetailView: View {
-    @Binding var name: String
-    @Binding var isPublic: Bool
+struct BScreen: View {
+    
+    @Binding var count: Int
     
     var body: some View {
-        VStack(alignment: .leading, spacing: 7) {
-            Text(name)
-            Text(isPublic == true ? "True" : "False")
-            Spacer()
+        VStack {
+            Text("\(count)")
+            Stepper("Counter", value: $count)
+            Button("Reset") {
+                count = 0
+            }
         }
     }
 }
+
 
 #Preview {
     ContentView()
